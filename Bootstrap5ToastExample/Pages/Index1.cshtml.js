795160f91a1175fd521bc50495c8dec21533d088
Karen Payne <paynekaren@comcast.net>
2023-06-18 08:19:11 -0700

    bootstrap code refactors

---
+++ Bootstrap5ToastExample/Pages/Index1.cshtml.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+
+    console.log(document.getElementById('navBar').offsetHeight);
+    console.log(document.querySelector('body').offsetHeight);
+    console.log(document.querySelector('footer').offsetHeight);
+
+    $("#toastButton1").click(function () {
+        $("#toast1").toast("show");
+    });
+});
---
+++ Bootstrap5ToastExample/Pages/Privacy.cshtml.js
@@ -0,0 +1,14 @@
+﻿document.getElementById("toastButton1").onclick = function () {
+    var toastElList = [].slice.call(document.querySelectorAll('#topToast'));
+    var toastList = toastElList.map(function (toastEl) {
+        return new bootstrap.Toast(toastEl);
+    });
+    toastList.forEach(toast => toast.show());
+}
+document.getElementById("toastButton2").onclick = function () {
+    var toastElList = [].slice.call(document.querySelectorAll('#bottomToast'));
+    var toastList = toastElList.map(function (toastEl) {
+        return new bootstrap.Toast(toastEl);
+    });
+    toastList.forEach(toast => toast.show());
+}
